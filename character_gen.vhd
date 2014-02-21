7c973e205ecae61910deb3790c1957ed22ae28d7
John Miller <c15john.miller@usafa.edu>
2014-02-21 10:52:49 -0700

    Have wired up everything in the character gen with the exception of the mux and the row DFF, nothing tested or syntaxed yet

--- character_gen.vhd
+++ character_gen.vhd
@@ -49,17 +49,62 @@ architecture Behavioral of character_gen is
 		);
 	END COMPONENT;
 
+	COMPONENT char_screen_buffer
+	PORT(
+		clk : IN std_logic;
+		we : IN std_logic;
+		address_a : IN std_logic_vector(11 downto 0);
+		address_b : IN std_logic_vector(11 downto 0);
+		data_in : IN std_logic_vector(7 downto 0);          
+		data_out_a : OUT std_logic_vector(7 downto 0);
+		data_out_b : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
+
+
 signal address : std_logic_vector(10 downto 0);
 signal rom_data : std_logic_vector(7 downto 0);
-
+signal data_b_sig : std_logic_vector(6 downto 0);
+signal row_reg, row_next : std_logic_vector(10 downto 0);
+signal font_data_sig : std_logic_vector(7 downto 0);
+signal col_reg, col_next_1, col_next_2 : std_logoc_vector(3 downto 0);
 
 begin
 	
-	Inst_font_rom: font_rom PORT MAP(
-		clk => clk ,
-		addr => ,
-		data => 
+
+Inst_char_screen_buffer: char_screen_buffer PORT MAP(
+		clk => clk,
+		we => write_en,
+		address_a => (others => '0') ,
+		address_b => open,
+		data_in => ascii_to_write,
+		data_out_a => open,
+		data_out_b => data_b_sig
 	);
+
+Inst_font_rom: font_rom PORT MAP(
+		clk => clk ,
+		addr => row_next & data_b_sig,
+		data =>  font_data_sig
+	);
+
+
+process(clk) is 
+begin
+if(rising_edge(clk)) then
+	col_next_1 <= (column(2) & column(1) & column(0));
+	end if;
+end process;
+
+process(clk) is
+begin
+if(rising_edge(clk)) then
+	col_next_2 <= col_next_1;
+	end if;
+end process;
+
+col_reg <= col_next_2;
+
 
 
 
